6167915b9c5c884c480aea44d4a9dea81ab7908a
randomguy70 <onlychessfreeze0515@gmail.com>
2022-01-14 21:31:00 -0600

    updated

--- data_blocks.c
+++ data_blocks.c
@@ -12,6 +12,8 @@ int shiftCol(struct DataBlock *block, int col, int ticks, enum ShiftDir dir)
 	// 1101 0100 0101 1010
 	// 0101 1010 1101 0100
 	// 1101 0100 0101 1010
+	// 1101 0100 0101 1010
+	// 1101 0100 0101 1010
 	
 	// copy the column to preserve the wrapped bytes
 	for(int i=0; i<block->height; i++)
@@ -19,13 +21,42 @@ int shiftCol(struct DataBlock *block, int col, int ticks, enum ShiftDir dir)
 		tempCol[i] = getByte(block, col, i);
 	}
 	
-	// shift down XXX (I haven't finished it)
-	if(dir == 1)
+	// shift down (untested)
+	if(dir == SHIFT_DOWN)
 	{
-		for(int i=block->height - ticks; i > -1; i-=ticks)
+		for(int i=block->height - ticks - 1; i >= 0; i-=1)
 		{
 			Byte byte = getByte(block, col, i);
-			setByte(byte, block, col, i+1);
+			setByte(byte, block, col, i+ticks);
+		}
+		// wrapped bits
+		for(int i=ticks - 1, j = block->height - 1; i >=0; i--, j--)
+		{
+			Byte byte = getByte(block, col, j);
+			setByte(byte, block, col, i);
+		}
+	}
+	
+	// 0101 1010 1101 0100
+	// 1101 0100 0101 1010
+	// 0101 1010 1101 0100
+	// 1101 0100 0101 1010
+	// 1101 0100 0101 1010
+	// 1101 0100 0101 1010
+	
+	// shift up XXX (unfinished)
+	else if(dir == SHIFT_UP)
+	{
+		for(int i=ticks; i < block->height - 1; i++)
+		{
+			Byte byte = getByte(block, col, i);
+			setByte(byte, block, col, i-ticks);
+		}
+		// wrapped bits
+		for(int i=ticks - 1, j = block->height - 1; i >=0; i--, j--)
+		{
+			Byte byte = getByte(block, col, j);
+			setByte(byte, block, col, i);
 		}
 	}
 	
