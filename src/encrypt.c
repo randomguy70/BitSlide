5fdd3f7def63ed59da5e819d2fb620e419dc4cd2
randomguy70 <onlychessfreeze0515@gmail.com>
2022-02-04 13:18:32 -0600

    fixed bug?

--- src/encrypt.c
+++ src/encrypt.c
@@ -28,8 +28,8 @@ struct Data *encryptData(struct Data *data, char *key)
 	// }
 
 	block1 = dataToBlocks(data);
-	shiftCol(block1, 0, 1, SHIFT_UP);
-	shiftCol(block1, 0, 1, SHIFT_DOWN);
+	shiftCol(block1, 0, 2, SHIFT_DOWN);
+	shiftCol(block1, 0, 2, SHIFT_UP);
 
 	ret = blocksToData(block1);
 
